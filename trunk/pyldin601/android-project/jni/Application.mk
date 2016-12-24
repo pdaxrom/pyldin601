50dda02c9b078448d2187386a794089dbd679bb9
sash <sash@876863e5-e8e5-4757-9d6e-8bdb005fdef8>
2016-12-24 17:15:32 +0000

    build script

--- trunk/pyldin601/android-project/jni/Application.mk
+++ trunk/pyldin601/android-project/jni/Application.mk
@@ -3,8 +3,8 @@
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
 # APP_STL := stlport_static 
 
-APP_ABI := armeabi-v7a armeabi arm64-v8a
-# x86 x86_64 mips mips64
+APP_ABI := all
+#armeabi-v7a armeabi arm64-v8a x86 x86_64 mips mips64
 
 # Min SDK level
 APP_PLATFORM=android-10
