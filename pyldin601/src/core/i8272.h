4db7552f3795437824bc25648992eab571f00111
sashz <23256427+pdaxrom@users.noreply.github.com>
2017-06-19 19:48:30 +0000

    stable 2

--- pyldin601/src/core/i8272.h
+++
@@ -1,9 +0,0 @@
-#ifndef _I8272_H_
-#define _I8272_H_
-
-void i8272Init(void);
-
-void i8272WriteByte(byte a, byte d);
-byte i8272ReadByte(byte a);
-
-#endif
--- pyldin601/te-lpc2478/diskovod.h
+++
@@ -1,6 +0,0 @@
-#ifndef _DISKOVOD_H_
-#define _DISKOVOD_H_
-
-void floppy_power_timeout(void);
-
-#endif
