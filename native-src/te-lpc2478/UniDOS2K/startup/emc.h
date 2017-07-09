8a663ea06ee13d01c4940f28e9b3f57b8c77d7d4
sashz <23256427+pdaxrom@users.noreply.github.com>
2017-07-09 08:33:49 +0000

    pinouts compatible with home made wired prototype board

--- native-src/te-lpc2478/UniDOS2K/startup/emc.h
+++
@@ -1,6 +0,0 @@
-#ifndef _EMC_H_
-#define _EMC_H_
-
-void Init_EMC(void);
-
-#endif
--- native-src/te-lpc2478/UniDOS2K/startup/loadelf.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ELF_H_
-#define _ELF_H_
-
-#define ELF_NO_FILE	(-1000)
-
-int load_elf(void *addr, char *arg, void *entry);
-
-#endif
